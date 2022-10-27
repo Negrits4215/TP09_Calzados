6456db889905b24de60aeb585ff1940ee0188086
Negrits4215 <115021032+Negrits4215@users.noreply.github.com>
2022-10-27 08:48:04 -0300

    Create .NET MVC Project

---
+++ Models/Calzado.cs
@@ -0,0 +1,43 @@
+using System;
+namespace tp09.Models;
+
+/*public class Calzado
+{
+    private int _idCalzado; private int _precio; private int _talle; private string _color; private string _marca; private int _stock; private DateTime _fechaInicioVenta;
+
+    public Calzado () {}
+
+    public Calzado (int pidCalzado, int pprecio, int ptalle, string pcolor, string pmarca, int pstock, DateTime pfechaInicioVenta)
+    {
+        _idCalzado = pidCalzado; _precio = pprecio; _talle = ptalle; _color = pcolor; _marca = pmarca; _stock = pstock; _fechaInicioVenta = pfechaInicioVenta;
+    }
+
+    public int idCalzado
+    {
+
+    }
+    public int _precio
+    {
+
+    }
+    public int _talle
+    {
+
+    }
+    public string _color
+    {
+
+    }
+    public string _marca
+    {
+
+    }
+    public int _stock
+    {
+
+    }
+    public DateTime _fechaInicioVenta
+    {
+        
+    }
+}*/
